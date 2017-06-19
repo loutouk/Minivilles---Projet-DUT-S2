5ec4dbb8c6c69952d631326ae2376246376616ff
ouinon <valentin76133@gmail.com>
2017-06-19 16:44:35 +0200

    : à la place de .

--- src/minivilles/metier/cartes/Stade.java
+++ src/minivilles/metier/cartes/Stade.java
@@ -3,7 +3,7 @@ package minivilles.metier.cartes;
 public class Stade extends Carte {
 
 	public Stade (String identifiant, String nom, String couleur, int declencheur, int cout) {
-		super("6.1", "Stade", "Violet", 6,6);
+		super("6:1", "Stade", "Violet", 6,6);
 	}
 	
 	public void lancerEffet(Metier m) {
