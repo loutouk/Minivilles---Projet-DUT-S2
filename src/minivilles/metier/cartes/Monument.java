5acfa17bd1aa2ff3ce13b21fe314d557fee2176a
BrummbaerFr <richard.blondel2@laposte.net>
2017-06-19 16:19:10 +0200

    Déplacement de la classe Monument, déclaration de Monument en abstraite.

--- src/minivilles/metier/cartes/Monument.java
+++ src/minivilles/metier/cartes/Monument.java
@@ -1,11 +1,9 @@
-package minivilles.metier;
-
-import minivilles.metier.cartes.Carte;
+package minivilles.metier.cartes;
 
 /**
  * Created by richard on 6/19/17.
  */
-public class Monument extends Carte {
+public abstract class Monument extends Carte {
 	boolean estVisible = false;
 
 	public Monument(String identifiant, String nom, String couleur, int declencheur, int cout) {
@@ -15,4 +13,6 @@ public class Monument extends Carte {
 	public boolean estVisible() {
 		return this.estVisible;
 	}
+
+	public abstract boolean lancerEffet();
 }
