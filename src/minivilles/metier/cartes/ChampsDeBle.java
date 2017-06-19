32500caffea245f62b8d66c5eba11dae60c35757
ouinon <valentin76133@gmail.com>
2017-06-19 16:58:36 +0200

    champs foret fabrique

---
+++ src/minivilles/metier/cartes/ChampsDeBle.java
@@ -0,0 +1,15 @@
+package minivilles.metier.cartes;
+
+import minivilles.metier.Metier;
+
+public class ChampsDeBle extends Carte {
+
+	public ChampsDeBle() {
+		super("1", "Champs de blé", "bleu", 1, 1);
+	}
+
+	@Override
+	public void lancerEffet(Metier metier) {
+
+	}
+}
---
+++ src/minivilles/metier/cartes/Foret.java
@@ -0,0 +1,15 @@
+package minivilles.metier.cartes;
+
+import minivilles.metier.Metier;
+
+public class Foret extends Carte {
+
+	public Foret() {
+		super("5", "Champs de blé", "bleu", 5, 3);
+	}
+
+	@Override
+	public void lancerEffet(Metier metier) {
+
+	}
+}
