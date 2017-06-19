6a00567b864f655df7d7e1191bda05a8142aedb0
BrummbaerFr <richard.blondel2@laposte.net>
2017-06-19 11:04:25 +0200

    Ajout structure (modèle MVC).

---
+++ src/minivilles/Controleur.java
@@ -0,0 +1,7 @@
+package minivilles;
+
+/**
+ * Created by richard on 6/19/17.
+ */
+public class Controleur {
+}
---
+++ src/minivilles/metier/Carte.java
@@ -0,0 +1,17 @@
+package minivilles.metier;
+
+/**
+ * Created by richard on 6/19/17.
+ */
+public class Carte {
+	String nom;
+
+	int declencheur;
+	int cout;
+
+	public Carte(String nom, int declencheur, int cout) {
+		this.nom = nom;
+		this.declencheur = declencheur;
+		this.cout = cout;
+	}
+}
