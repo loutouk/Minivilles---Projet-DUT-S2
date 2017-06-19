06ae6ee90c5c946daf3ceed984b4f8e846e21f3a
BrummbaerFr <richard.blondel2@laposte.net>
2017-06-19 16:49:21 +0200

    Création des classes Fromagerie, MarcheDeFruitsEtLegumes, et Superette.

---
+++ src/minivilles/metier/cartes/Fromagerie.java
@@ -0,0 +1,18 @@
+package minivilles.metier.cartes;
+
+import minivilles.metier.Metier;
+
+public class Fromagerie extends Carte {
+
+	public Fromagerie() {
+		super(
+				"7", "Fromagerie",
+				"Vert", 7, 5
+		);
+	}
+
+	@Override
+	public void lancerEffet(Metier metier) {
+
+	}
+}
